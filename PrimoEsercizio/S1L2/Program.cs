995f6ec466f85dba034c27e7a0576ef846f576dc
Federico4820 <federicolepore1@gmail.com>
2025-02-11 16:17:41 +0100

    S1L2

---
+++ PrimoEsercizio/S1L2/Program.cs
@@ -0,0 +1,10 @@
+﻿using S1L2.ogetti;
+
+Persona persona = new Persona()
+{
+    Nome = "Federico",
+    Cognome = "Lepore",
+    Età = 21
+};
+
+persona.MostraPersona();
---
+++ PrimoEsercizio/S1L2/ogetti/persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1L2.ogetti
+{
+    public class Persona
+    {
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public int? Età { get; set; }
+
+        public void MostraPersona()
+        {
+            Console.WriteLine("Ciao mi chiamo " + Nome + " " + Cognome + " ed ho " + Età + " anni");
+        }
+    }
+
+}
